fbd0ebf08ec892516f77ab416100886784b96fea
martinlotufo93 <martinlotufo93@gamil.com>
2022-04-16 13:32:02 -0300

    BaSP-2022: variables_and_operators exercise

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,16 @@
+var none = 5
+var ntwo = 2
+var nresult = none + ntwo
+console.log (nresult)
+
+var sone = "first"
+var stwo = "concatenation"
+var sresult = sone + stwo
+console.log (sone+ " " + stwo)
+
+var sthree = "length"
+var sfour = "calculation"
+var lresult = sthree + sfour
+var logresult = lresult.length
+console.log (logresult)
+
