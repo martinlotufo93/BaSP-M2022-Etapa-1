482dad9b3fd137651b28ce48c8931ae7dacdc409
martinlotufo93 <martinlotufo93@gamil.com>
2022-04-17 13:44:41 -0300

    BaSP-2022: arrays exercise

---
+++ Semana-05/exercises/03_arrays.js
@@ -0,0 +1,40 @@
+//Point A
+var year = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+console.log(year[4]);
+console.log(year[10]);
+
+
+//Point B
+var yearb = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+var orderyear = yearb.sort();
+console.log(orderyear);
+
+//Point C
+var yearc = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+yearc.unshift("Arbol");
+console.log (yearc);
+var yeard = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+yeard.push("Gallo");
+console.log (yeard);
+
+//Point D
+var yeare = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+yeare.shift();
+console.log (yeare);
+var yearf = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+yearf.pop();
+console.log (yearf);
+
+//Point E
+var yearg = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+yearg.reverse();
+console.log (yearg);
+
+//Point F
+var yearh = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+console.log(yearh.join('-'));
+
+//Point G
+var yeari = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+var shortyear = yeari.slice(4,11);
+console.log (shortyear);
