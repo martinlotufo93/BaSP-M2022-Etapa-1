a493ee96da8aae0299ca071e22f7ec6a02c58920
martinlotufo93 <martinlotufo93@gamil.com>
2022-04-26 20:33:46 -0300

    BaSP-2022: Js Sign-up etapa3

--- Semana-06/javascript/sign-up.js
+++ Semana-06/javascript/sign-up.js
@@ -1,4 +1,3 @@
-
 function validation(input,regex){
 
     console.log(input);
@@ -11,9 +10,15 @@ function validation(input,regex){
 
         console.log("error");
 
+        return false;
+
     }else{
+
+        elemento.style.backgroundColor='green';
         
         console.log("ok");
+
+        return true;
 
     }
 
@@ -39,8 +44,56 @@ function checktel(){
     validation("phone",/^([0-9]{10,})$/);
 
 }
+function checkweek(){
+
+    validation("week",/^([0-9]{2}[/][0-9]{2}[/][0-9]{4})$/);
+
+}
+function checkpassword(){
+
+    validation("password",/^([a-zA-Z0-9]{8,})$/);
 
 
+}
+function checklocation(){
+
+    validation("location",/^([a-zA-Z0-9]{3,})$/);
 
 
+}
+function checkpostalcode(){
 
+    validation("postal-code",/^([0-9]{4,5})$/);
+
+
+}
+function checkemail(){
+
+    validation("email",/^([\da-z_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/);
+
+
+}
+function checkaddress(){
+
+    validation("address",/^([a-zA-Z0-9 ]{5,})$/);
+
+}
+
+function checkrepassword(){
+
+    var password = document.getElementById("password");
+    var repassword = document.getElementById("re-password");
+    if(password.value == repassword.value){
+        repassword.style.backgroundColor='';
+        console.log ("coinciden");
+    }
+        else{
+        repassword.style.backgroundColor='red';
+        console.log ("no coinciden");
+    }
+
+}
+
+function formstatus(){
+
+}
