f147f2448fa9624b6deaa7f7baa18463be8a92bd
martinlotufo93 <martinlotufo93@gamil.com>
2022-05-02 12:15:17 -0300

    BaSP-2022: log in corrections

--- Semana-07/javascript/login.js
+++ Semana-07/javascript/login.js
@@ -1,32 +1,68 @@
+function CheckMail(){
+    element = document.getElementById("email");
+    if(element.value == 'rose@radiumrocket.com'){
+        document.getElementById('email').style.backgroundColor='green';
+        document.getElementById("message-error-email").style.display="none";
+    }else{
+        document.getElementById('email').style.backgroundColor='red';
+        document.getElementById("message-error-email").innerHTML="The email is not correct.";
+        document.getElementById("message-error-email").style.display="block";
+    }
+}
 
-    function CheckMail(){
-        regex=/^([\da-z_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/;
-        var elemento = document.getElementById("email");
-            if(!regex.exec(elemento.value)){
-                document.getElementById('email').style.backgroundColor='red';
-                document.getElementById("message-error-email").innerHTML="Error";
-                document.getElementById("message-error-email").style.display="block";
-            }else{
-                document.getElementById('email').style.backgroundColor='green';
-                document.getElementById("message-error-email").style.display="none";
+function CheckPassword(){
+    element = document.getElementById("password");
+    if(element.value == 'BaSP2022'){
+        document.getElementById('password').style.backgroundColor='green';
+        document.getElementById("message-error-password").style.display="none";
+    }else{
+        document.getElementById('password').style.backgroundColor='red';
+        document.getElementById("message-error-password").innerHTML="The password is not correct.";
+        document.getElementById("message-error-password").style.display="block";
+    }
+}
 
-            }
-        }
+function login (){
+    var elementemail = document.getElementById("email");
+    var elementpassword = document.getElementById("password");
+    if((elementemail.value == 'rose@radiumrocket.com') && (elementpassword.value == 'BaSP2022')){
+        alert("Everything's fine" + " - The email entered is correct: " + elementemail.value + " The entered password is correct: " + elementpassword.value);
+    }
+    else if(elementemail.value != 'rose@radiumrocket.com'){
+        alert("The email entered is not correct:");
+    }else(elementpassword.value != 'BaSP2022');{
+        alert("The entered password is not correct:");
+    }
+    return false;
+}   
+/*function CheckMail(){
+    regex=/^([\da-z_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/;
+    var elemento = document.getElementById("email");
+    if(!regex.exec(elemento.value)){
+        document.getElementById('email').style.backgroundColor='red';
+        document.getElementById("message-error-email").innerHTML="Error";
+        document.getElementById("message-error-email").style.display="block";
+    }else{
+        document.getElementById('email').style.backgroundColor='green';
+        document.getElementById("message-error-email").style.display="none";
 
-    function CheckPassword(){
-        regex=/^([a-zA-Z0-9]{8,})$/;
-        var elemento = document.getElementById("password");
-            if(!regex.exec(elemento.value)){
-                document.getElementById('password').style.backgroundColor='red';
-                document.getElementById("message-error-password").innerHTML="Error";
-                document.getElementById("message-error-password").style.display="block";
-            }else{
-                document.getElementById('password').style.backgroundColor='green';
-                document.getElementById("message-error-password").style.display="none";
-            }
-        }
+    }
+}*/
 
-    function login (){
+/*function CheckPassword(){
+    regex=/^([a-zA-Z0-9]{8,})$/;
+    var elemento = document.getElementById("password");
+    if(!regex.exec(elemento.value)){
+        document.getElementById('password').style.backgroundColor='red';
+        document.getElementById("message-error-password").innerHTML="Error";
+        document.getElementById("message-error-password").style.display="block";
+        }else{
+        document.getElementById('password').style.backgroundColor='green';
+        document.getElementById("message-error-password").style.display="none";
+    }
+}*/
+
+/*    function login (){
         regexemail=/^([\da-z_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/;
         var elementemail = document.getElementById("email");
         regexpassword=/^([a-zA-Z0-9]{8,})$/;
@@ -42,4 +78,4 @@
                 alert("La contraseña no cumple con los requisitos");
             }
             return false;
-    }       
+    } */     
